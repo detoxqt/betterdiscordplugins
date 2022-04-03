cee8f22af364642dd067f3390a33de97610702ec
JikaiSH <79759440+JikaiSH@users.noreply.github.com>
2022-04-03 11:33:27 +0200

    started !

---
+++ JKServerCounter.plugin.js
@@ -0,0 +1,15 @@
+/**
+ * @name JKServerCounter
+ * @author JikaiSH
+ * @version 0.1.0
+ * @description See how many server you are in !
+ */
+
+module.exports = class MyPlugin {
+    start() {
+        // do something on plugin start
+    }
+    stop() {
+        // do something on plugin stop
+    }
+};
